c0f8863ef5278278c47eb654f9c086f5e0642419
Baki Altun <bakialtun@gonder-al.com>
2022-03-30 17:41:02 +0300

    prod index redirection part 1

---
+++ src/Presentation/Api/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Boyner.CaseStudy.ApplicationCore.Commands;
+using Boyner.CaseStudy.ApplicationCore.Entities;
+using Boyner.CaseStudy.ApplicationCore.Queries.PostQueries;
+using Boyner.CaseStudy.Infrastructure.RabbitMQ;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Boyner.CaseStudy.Presentation.Api.Controllers
+{ 
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        private readonly IRabbitMQ<Post> _mqueue;
+        private readonly IMediator _meditor;
+        public HomeController(IRabbitMQ<Post> mq, IMediator meditor)
+        {
+            _mqueue = mq;
+            _meditor = meditor;
+        }
+
+
+        public IActionResult Index()
+        {
+            return Redirect("/index.html");
+        }
+    }
+}
